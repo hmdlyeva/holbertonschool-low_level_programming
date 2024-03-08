ec567398922e4f50ffef4e38e84bd1e47d00a247
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-03-08 08:57:50 +0000

    task 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+	char *start = s;
+
+	while (i < n)
+	{
+		*s++ = b;
+		i++;
+	}
+	return (start);
+}
