9ec8eae08de50562512378dd19edbf07ecb21a42
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-03-08 09:52:38 +0000

    rec

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,6 +3,7 @@
  * sqrt2 - Makes possible to evaluate from 1 to n
  * @a: same number as n
  * @b: number that iterates from 1 to n
+ * Return: On success 1.
  */
 int sqrt2(int a, int b)
 {
@@ -12,7 +13,13 @@ int sqrt2(int a, int b)
 		return (-1);
 	return (sqrt2(a, b + 1));
 }
-
+/**
+ * _sqrt_recursion - returns the natural square root of n
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _sqrt_recursion(int n)
 {
 	return (sqrt2(n, 1));
