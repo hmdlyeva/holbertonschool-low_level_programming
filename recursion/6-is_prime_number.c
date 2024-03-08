0ecef8586a1979ffe6847a15e108f57a3a835fa0
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-03-08 09:54:14 +0000

    rec

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * prime2 - Makes possible to evaluate from 1 to n
@@ -16,6 +16,13 @@ int prime2(int a, int b)
 		return (0);
 	return (prime2(a, b + 1));
 }
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int is_prime_number(int n)
 {
 	if (n <= 1)
