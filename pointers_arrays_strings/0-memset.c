5a7f27e1a4211103771f19e0098241107f2586ca
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-03-08 09:04:29 +0000

    task

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,13 @@
 #include "main.h"
-
+/**
+ * *_memset - Fills memory with a constant byte
+ *
+ * @s: pointer
+ * @b: char
+ * @n: unsigned int
+ *
+ *Return: s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
