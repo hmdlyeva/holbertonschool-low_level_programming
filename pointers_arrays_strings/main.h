2239a0089bd9a868d2077c6709403be757ede13a
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-03-08 09:16:22 +0000

    task

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -26,4 +26,4 @@ int _putchar(char);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n);
