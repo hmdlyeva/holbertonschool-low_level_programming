4a0398f0170beed939f2bba80df09bfdf257f9a0
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-03-08 10:11:46 +0000

    task

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - function to print multiplication result
- * @argc: number of arguments
- * @argv: arguments supplied
- * Return: always 1 if less than two arg var supplied otherwise 0.
+ * main - Entry point
+ *
+ * @argc: incoming argument
+ * @argv: incoming argument
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char **argv)
-{
-	int a = 0, b = 0;
-
-	if (argc > 2)
 	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		printf("%d\n", a * b);
-	}
-	else
+
+	 int total;
+
+	if (argc < 2)
 	{
 		printf("Error\n");
+		return (1);
 	}
+
+	total = (int) *argv[1] * (int) *argv[2];
+
+	printf("%d\n", total);
+
 	return (0);
-}
+
+	}
