6aab9dc3f73c0849e01bba0892b97ec4f764e02c
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-03-21 09:19:01 +0000

    tasks

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,35 +1,47 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * new_dog - entry point
- * @name: string from main, name of pet
- * @age: number from main, age of pet
- * @owner: string from main, owner of pet
- * Return: p
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: pointer to the new dog struct, or NULL if failed
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
-	/* reserving memory to struct*/
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
-		return (NULL);
-	/* Cpunting name pointer*/
-	if (name == NULL)
-	{
-		free(p);
-		free(owner);
-		return (NULL);
-	}
-	if (owner == NULL)
-	{
-		free(p);
-		free(name);
-		return (NULL);
-	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+    dog_t *p;
+    char *name_copy, *owner_copy;
+
+    
+    p = malloc(sizeof(dog_t));
+    if (p == NULL)
+        return (NULL);
+
+    
+    name_copy = malloc(strlen(name) + 1);
+    if (name_copy == NULL)
+    {
+        free(p);
+        return (NULL);
+    }
+    strcpy(name_copy, name);
+
+    
+    owner_copy = malloc(strlen(owner) + 1);
+    if (owner_copy == NULL)
+    {
+        free(name_copy);
+        free(p);
+        return (NULL);
+    }
+    strcpy(owner_copy, owner);
+
+    
+    p->name = name_copy;
+    p->age = age;
+    p->owner = owner_copy;
+
+    return (p);
 }
